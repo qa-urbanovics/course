ba79656d100fc2d15fa9bbdcdbf63410469e0d8c
qa-urbanovics <aleksejs.urbanovics@gmail.com>
2020-06-28 14:25:02 +0300

    delfi tests

--- src/test/java/DelfiTests.java
+++ src/test/java/DelfiTests.java
@@ -1,9 +1,12 @@
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DelfiTests {
     @Test
 
-    public void firstDelfiTests{
-
+    public void firstDelfiTest() {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rus.delfi.lv");
     }
 }
