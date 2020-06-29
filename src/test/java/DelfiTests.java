2651ad12a95220dad25c123e2dd3b3d50161287f
qa-urbanovics <aleksejs.urbanovics@gmail.com>
2020-06-29 15:11:49 +0300

    add open browser maximize size :)

--- src/test/java/DelfiTests.java
+++ src/test/java/DelfiTests.java
@@ -8,6 +8,7 @@ public class DelfiTests {
     public void firstDelfiTest() {
         System.setProperty("webdriver.chrome.driver", "c://chromedriver.exe");
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.get("https://rus.delfi.lv");
     }
 }
