849a1b2da81d2d3d9d552248b90c36985ea1bc93
qa-urbanovics <aleksejs.urbanovics@gmail.com>
2020-07-12 15:08:30 +0300

    Delfi Show First Headline

--- src/test/java/DelfiTests.java
+++ src/test/java/DelfiTests.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -10,5 +11,8 @@ public class DelfiTests {
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://rus.delfi.lv");
+
+       String text = driver.findElement(By.xpath(".//h1[contains(@class,'headline__title')]")).getText();
+       System.out.println(text);
     }
 }
