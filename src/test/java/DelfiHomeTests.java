24d2320449ab95ebaa731c29ec0550e122643637
qa-urbanovics <aleksejs.urbanovics@gmail.com>
2020-07-18 15:36:22 +0300

    second home work

---
+++ src/test/java/DelfiHomeTests.java
@@ -0,0 +1,31 @@
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DelfiHomeTests {
+    @Test
+    public void FirstSiteTitle(){
+        System.setProperty("webdriver.chrome.driver", "c://chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://rus.delfi.lv");
+
+        String text = driver.findElement(By.xpath(".//h1[contains(@class,'headline__title')]")).getText();
+        System.out.println("Delfi last article title is:" + text);
+
+        String comment = driver.findElement(By.xpath(".//a[contains(@class,'comment-count text-red-ribbon')]")).getText();
+        comment = comment.replace("(", "").replace(")", "");
+        System.out.println("Comment count:" + comment);
+
+        String title = new String("Dmitrijs from QA GURU WIN one mln EUR!!!");
+
+        assertEquals(title,text);
+
+
+
+
+    }
+}
