dba3e2c27ed799f3d102dd7276e7c4f64ba8012f
qa-urbanovics <aleksejs.urbanovics@gmail.com>
2020-08-23 11:14:03 +0300

    Delfi title and comment check

--- src/test/java/DelfiTitleAndCommentCheck.java
+++ src/test/java/DelfiTitleAndCommentCheck.java
@@ -1,3 +1,5 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
@@ -18,48 +20,70 @@ public class DelfiTitleAndCommentCheck {
     private final By ARTICLE_PAGE_COMMENTS = By.xpath(".//p[contains(@class, 'text-counter')]");
     private final By COMMENTS_PAGE_COUNT = By.xpath(".//span[contains(@class, 'article-comments')]");
 
+    private final Logger LOGGER = LogManager.getLogger(this.getClass());
+
+
     @Test
 
     public void articleTitleAndCommentCheck(){
+
+        LOGGER.info("This test is checking titles and comment count");
         System.setProperty("webdriver.chrome.driver", "c://chromedriver.exe");
+
+        LOGGER.info("Opening browser");
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
+
+        LOGGER.info("Opening delfi page");
         driver.get("https://rus.delfi.lv");
 
-        //find articles
+        LOGGER.info("Getting all articles");
         List<WebElement> articles = driver.findElements(ARTICLE);
-        //find 3rd
+
+        LOGGER.info("Find 3rd articles");
         WebElement article = articles.get(2);
-        //find title
+
+        LOGGER.info("Getting title");
         String homePageTitle = article.findElement(TITLE).getText();
-        //find comments counts
+
+        LOGGER.info("find Comments counts");
         int homePageCommentsCount = 0;
         if (!article.findElements(COMMENTS_COUNT).isEmpty()) {
             homePageCommentsCount = parseCommentCount(article.findElement(COMMENTS_COUNT).getText());
         }
         // debug
         // System.out.println(homePageTitle + " " + homePageCommentsCount);
-        //click on it
+
+        LOGGER.info("Click on article title");
         article.findElement(TITLE).click();
-        //Wait for title
-        WebDriverWait wait = new WebDriverWait(driver, 10);
+
+        LOGGER.info("Waiting for title");
+        WebDriverWait wait = new WebDriverWait(driver, 15);
         wait.until(ExpectedConditions.visibilityOfElementLocated(ARTICLE_PAGE_TITLE));
-        //find title
+
+        LOGGER.info("Find article page title");
         String articlePageTitle = driver.findElement(ARTICLE_PAGE_TITLE).getText();
-        //find comment count
+
+        LOGGER.info("Find article page comments");
         int articlePageCommentCount = Integer.parseInt(driver.findElement(ARTICLE_PAGE_COMMENTS).getText());
-        //check
+
+        LOGGER.info("Check article and comment");
         Assertions.assertTrue(homePageTitle.startsWith(articlePageTitle), "Wrong title1!");
         Assertions.assertEquals(homePageCommentsCount, articlePageCommentCount, "Wrong comment count1");
-        //open comment page
+
+        LOGGER.info("Open comment page");
         driver.findElement(ARTICLE_PAGE_COMMENTS).click();
-        //wait for title
+
+        LOGGER.info("Waiting article page title");
         wait.until(ExpectedConditions.visibilityOfElementLocated(ARTICLE_PAGE_TITLE));
-        //find title
+
+        LOGGER.info("Find artile title");
         String commentsPageTitle = driver.findElement(ARTICLE_PAGE_TITLE).getText();
-        //find comments count
+
+        LOGGER.info("Find comment counts");
         int commentsPageCommentsCount = Integer.parseInt(driver.findElement(COMMENTS_PAGE_COUNT).getText());
-        //check
+
+        LOGGER.info("Check title and comment count in comment page");
         Assertions.assertTrue(homePageTitle.startsWith(commentsPageTitle), "Wrong title2!");
         Assertions.assertEquals(homePageCommentsCount, commentsPageCommentsCount, "Wrong comment2!");
     }
